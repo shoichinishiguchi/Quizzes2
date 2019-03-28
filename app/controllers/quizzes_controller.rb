76257b4e57c056f9c82028d37b9d9f855b6c0a85
shoichinishiguchi <shoichi2228@yahoo.co.jp>
2019-03-29 01:34:56 +0900

    For sleep

--- app/controllers/quizzes_controller.rb
+++ app/controllers/quizzes_controller.rb
@@ -4,6 +4,7 @@ class QuizzesController < ApplicationController
   def index
     @quizzes = Quiz.all.order('id DESC')
     @ranking = User.order('score DESC')
+    @current_user_rank = @ranking.index(current_user)+1 if user_signed_in?
   end
 
   def new
@@ -25,12 +26,23 @@ class QuizzesController < ApplicationController
     @quiz = Quiz.find(params[:id])
   end
 
+
+  def edit
+    @quiz = Quiz.find(params[:id])
+  end
+
+
   def answer
     if Quiz.find(params[:id]).answer == params[:answer] then
     current_user.score += Quiz.find(params[:id]).point
     end
     current_user.save
     @quiz = Quiz.find(params[:id])
+  end
+
+  def update
+    quiz = Quiz.find(params[:id])
+    quiz.update(text: params[:text], image: params[:image])
   end
 
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,18 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable
   has_many :quizzes
+
+def self.rank(rank)
+  User.order('score DESC')[rank-1]
+end
+#任意の順位のユーザーを取り出す。
+
+def rank_of(user)
+  ranking = User.order('score DESC')
+  return  ranking.index(user)+1
+end
+# なぜか、上のメソッドができない。@ranking = User.order('score DESC')がエラーに。
+
+
+
 end
